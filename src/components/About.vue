83e87ad67bacce68f46ef11f55acf7c733fbf004
Dika Seva <dikaseva@gmail.com>
2020-09-15 23:51:35 +0700

    update text at about section

--- src/components/About.vue
+++ src/components/About.vue
@@ -4,8 +4,8 @@
         <article>
             <h2>ABOUT</h2>
             <h3 class="title-summary">In a nutshell</h3>
-            <p>He is someone who loves clean and simple design with seamless interaction &amp; a pixel perfect mindset.</p>
-            <p>Has advance HTML and CSS experience, cares about decent designs, micro-interactions &amp; animations.</p>
+            <p>He is someone who loves a clean and simple design with seamless interaction &amp; a pixel-perfect mindset.</p>
+            <p>Has advanced HTML and CSS experience, cares about decent designs, micro-interactions &amp; animations.</p>
             <!-- <p>A CSS wizard, have strong ability to manipulates CSS &amp; HTML into amazing layouts with the most possible micro-interactions.</p> -->
             <p>A fan of <a href="https://www.smashingmagazine.com/" target="_blank">Smashing Magazine</a>, <a href="https://tympanus.net/codrops/" target="_blank">Codrops</a>, &amp; <a href="https://www.awwwards.com/" target="_blank">Awwwards Blog</a>.</p>
         </article>  
